3ff45bf20ebbecf810b53f2b9b2032a3bc922246
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2024-12-09 15:48:10 +0100

    Adding some visual styles

--- app/src/main/java/pl/sebcel/bpg/ui/measurementlist/MeasurementListTable.kt
+++ app/src/main/java/pl/sebcel/bpg/ui/measurementlist/MeasurementListTable.kt
@@ -1,12 +1,19 @@
 package pl.sebcel.bpg.ui.measurementlist
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import pl.sebcel.bpg.ui.theme.BPGTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pl.sebcel.bpg.data.local.database.Measurement
 import pl.sebcel.bpg.data.local.database.PainDescriptions
 import java.text.SimpleDateFormat
@@ -18,12 +25,28 @@ private val painDescriptions = PainDescriptions()
 
 @Composable
 fun MeasurementListTable(items: List<Measurement>, modifier: Modifier = Modifier) {
+    val shape = CircleShape
     Column(modifier) {
         items.forEach {
-            Row() {
-                Text(formatter.format(it.date))
+            Row(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .border(2.dp, MaterialTheme.colorScheme.secondary, shape)
+                    .background(MaterialTheme.colorScheme.primary, shape)
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ){
+                Text(
+                    formatter.format(it.date),
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
                 Text("\t")
-                Text(painDescriptions.getPainDescription(it.pain))
+                Text(
+                    painDescriptions.getPainDescription(it.pain),
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
             }
         }
     }
