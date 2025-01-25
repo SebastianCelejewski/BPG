8564b38d2596f7ac1460da8f1314bf06eb5a379b
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2025-01-25 13:28:34 +0100

    Initial export, of course working only on some devices, not on all

---
+++ app/src/main/java/pl/sebcel/bpg/export/ExcelExporter.kt
@@ -0,0 +1,77 @@
+package pl.sebcel.bpg.export
+
+import android.os.Environment
+import android.util.Log
+import jxl.DateCell
+import java.io.File
+
+import jxl.Workbook;
+import jxl.WorkbookSettings;
+import jxl.write.DateFormat
+import jxl.write.DateTime
+import jxl.write.Label;
+import jxl.write.Number
+import jxl.write.WritableCellFormat
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import pl.sebcel.bpg.BpgApplication
+import pl.sebcel.bpg.data.local.database.model.Measurement
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+object ExcelExporter {
+
+    private val fileNameDateFormatter = SimpleDateFormat("yyyy-MM-dd_HH-mm", Locale.UK)
+    private val cellDateFormatter = DateFormat("yyyy-MM-dd HH:mm")
+
+    fun export(measurements: List<Measurement>) {
+        Log.d("BPG", "Export to Excel started")
+        val sd = Environment.getExternalStorageDirectory()
+        val csvFile = "${fileNameDateFormatter.format(Date())} BPG export.xls"
+
+        val directory = File("${BpgApplication.instance.filesDir}/BPG")
+
+        Log.d("BPG", "filesdir: ${BpgApplication.instance.filesDir}")
+
+        //create directory if not exist
+        if (!directory.isDirectory) {
+            Log.d("BPG", "Creating export directory")
+            directory.mkdirs()
+        }
+
+        Log.d("BPG", "Directory exists: ${directory.exists()}")
+
+        try {
+            Log.d("BPG", "Directory path: $directory")
+            Log.d("BPG", "File name: $csvFile")
+            Log.d("BPG", "External storage status: ${Environment.getExternalStorageState()}")
+
+            val file = File(directory, csvFile)
+            val wbSettings: WorkbookSettings = WorkbookSettings()
+            wbSettings.setLocale(Locale(Locale.UK.getLanguage(), Locale.UK.getCountry()))
+            val workbook: WritableWorkbook = Workbook.createWorkbook(file, wbSettings)
+
+            val sheetA: WritableSheet = workbook.createSheet("Measurements", 0)
+
+            sheetA.addCell(Label(0, 0, "Data"))
+            sheetA.addCell(Label(1, 0, "Poziom bólu"))
+            sheetA.addCell(Label(2, 0, "Uwagi"))
+
+            var rowIdx = 1
+            measurements.forEach{measurement -> run {
+                //sheetA.addCell(Label(0, rowIdx, cellDateFormatter.format(measurement.date)))
+                sheetA.addCell(DateTime(0, rowIdx, measurement.date, WritableCellFormat(cellDateFormatter)))
+                sheetA.addCell(Number(1, rowIdx, measurement.pain.toDouble()))
+                sheetA.addCell(Label(2, rowIdx, measurement.comment))
+                rowIdx++
+            }}
+
+            workbook.write()
+            workbook.close()
+            Log.d("BPG", "Export completed")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
--- app/src/main/java/pl/sebcel/bpg/ui/measurementlist/MeasurementListActivity.kt
+++ app/src/main/java/pl/sebcel/bpg/ui/measurementlist/MeasurementListActivity.kt
@@ -1,11 +1,14 @@
 package pl.sebcel.bpg.ui.measurementlist
 
+import android.Manifest
 import android.content.Intent
-import androidx.compose.foundation.Image
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -22,20 +26,24 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import pl.sebcel.bpg.R
+import pl.sebcel.bpg.data.di.FakeMeasurementRepository
+import pl.sebcel.bpg.data.local.database.model.Measurement
+import pl.sebcel.bpg.export.ExcelExporter
+import pl.sebcel.bpg.services.dataupdate.DataUpdateServiceScheduler
 import pl.sebcel.bpg.ui.measurementadd.MeasurementAddActivity
 import pl.sebcel.bpg.ui.theme.BPGTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import pl.sebcel.bpg.data.di.FakeMeasurementRepository
-import pl.sebcel.bpg.services.dataupdate.DataUpdateServiceScheduler
 
 @AndroidEntryPoint
 class MeasurementListActivity : ComponentActivity() {
@@ -77,6 +85,15 @@ class MeasurementListActivity : ComponentActivity() {
                     FloatingActionButton(onClick = { AddMeasurement() }) {
                         Icon(Icons.Default.Add, contentDescription = getString(R.string.add_button_label))
                     }
+                    if (items is MeasurementListUiState.Success) {
+                        val measurements = (items as MeasurementListUiState.Success).data
+                        FloatingActionButton(onClick = { exportToExcel(measurements) }) {
+                            Icon(
+                                Icons.Default.Share,
+                                contentDescription = getString(R.string.export_button_label)
+                            )
+                        }
+                    }
                 }
             }
         ) {
@@ -85,7 +102,9 @@ class MeasurementListActivity : ComponentActivity() {
                 Image(
                     painter = painterResource(R.drawable.ornament),
                     contentDescription = stringResource(R.string.app_name),
-                    modifier = Modifier.fillMaxWidth().padding(12.dp)
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp)
                 )
                 Surface(
                     color = MaterialTheme.colorScheme.surface,
@@ -118,5 +137,37 @@ class MeasurementListActivity : ComponentActivity() {
     fun AddMeasurement() {
         val intent = Intent(Intent(baseContext, MeasurementAddActivity::class.java))
         startActivity(intent)
+    }
+
+    private fun askForPermissions(permissions: Array<String>, requestCode: Int) {
+        Log.d("BPG", "Checking permissions")
+        var permissionsToRequest : MutableList<String> = mutableListOf<String>()
+        permissions.forEach {
+            if (ContextCompat.checkSelfPermission(this@MeasurementListActivity, it) == PackageManager.PERMISSION_GRANTED) {
+                Log.d("BPG", "Permission $it is granted")
+            } else {
+                Log.d("BPG", "Permission $it is not granted")
+                permissionsToRequest.add(it)
+            }
+        }
+
+        Log.d("BPG", "Requesting permissions")
+        ActivityCompat.requestPermissions(this@MeasurementListActivity, permissions, requestCode)
+
+        Log.d("BPG", "Checking if permissions were granted")
+        permissions.forEach {
+            if (ContextCompat.checkSelfPermission(this@MeasurementListActivity, it) == PackageManager.PERMISSION_GRANTED) {
+                Log.d("BPG", "Permission $it is granted")
+            } else {
+                Log.d("BPG", "Permission $it is not granted")
+            }
+        }
+    }
+
+    private fun exportToExcel(measurements: List<Measurement>) {
+        val requestId = 1
+
+        askForPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), requestId)
+        ExcelExporter.export(measurements)
     }
 }
