9b91b7791ffa76b003add1728f019f523761f5da
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2025-01-26 12:13:14 +0100

    Final (probably) version of data export

--- app/src/main/java/pl/sebcel/bpg/export/ExcelExporter.kt
+++ app/src/main/java/pl/sebcel/bpg/export/ExcelExporter.kt
@@ -1,7 +1,15 @@
 package pl.sebcel.bpg.export
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Environment
 import android.util.Log
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import jxl.DateCell
 import java.io.File
 
@@ -14,7 +22,10 @@ import jxl.write.Number
 import jxl.write.WritableCellFormat
 import jxl.write.WritableSheet;
 import jxl.write.WritableWorkbook;
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import pl.sebcel.bpg.BpgApplication
+import pl.sebcel.bpg.R
 import pl.sebcel.bpg.data.local.database.model.Measurement
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -25,11 +36,61 @@ object ExcelExporter {
     private val fileNameDateFormatter = SimpleDateFormat("yyyy-MM-dd_HH-mm", Locale.UK)
     private val cellDateFormatter = DateFormat("yyyy-MM-dd HH:mm")
 
-    fun export(measurements: List<Measurement>) : String {
+    fun exportToExcel(measurements: List<Measurement>, snackbarHostState: SnackbarHostState, scope: CoroutineScope, activity: Activity) {
+        val requestId = 1
+
+        try {
+            askForPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), requestId, activity)
+            val fileName = saveToExcelFile(measurements)
+            scope.launch {
+                snackbarHostState.showSnackbar(message = activity.getString(R.string.export_completed_toast_text) + fileName, duration = SnackbarDuration.Long)
+            }
+            Log.d("BPG", "Export successful to file $fileName")
+        } catch (ex: Exception) {
+            scope.launch {
+                snackbarHostState.showSnackbar(message = activity.getString(R.string.export_failed_toast_text) + ex.message, duration = SnackbarDuration.Long)
+            }
+            Log.e("BPG", "Export failed: ${ex.message}", ex)
+        }
+    }
+
+    private fun askForPermissions(permissions: Array<String>, requestCode: Int, activity: Activity) {
+        Log.d("BPG", "Checking permissions")
+        val permissionsToRequest : MutableList<String> = mutableListOf<String>()
+        permissions.forEach {
+            if (ContextCompat.checkSelfPermission(activity, it) == PackageManager.PERMISSION_GRANTED) {
+                Log.d("BPG", "Permission $it is granted")
+            } else {
+                Log.d("BPG", "Permission $it is not granted")
+                permissionsToRequest.add(it)
+            }
+        }
+
+        Log.d("BPG", "Requesting permissions")
+        ActivityCompat.requestPermissions(activity, permissions, requestCode)
+
+        Log.d("BPG", "Checking if permissions were granted")
+        val permissionsNotGranted : MutableList<String> = mutableListOf()
+        permissions.forEach {
+            if (ContextCompat.checkSelfPermission(activity, it) == PackageManager.PERMISSION_GRANTED) {
+                Log.d("BPG", "Permission $it is granted")
+            } else {
+                Log.d("BPG", "Permission $it is not granted")
+                permissionsNotGranted.add(it)
+            }
+        }
+
+        if (permissionsNotGranted.isNotEmpty()) {
+            throw Exception("Failed to get permissions necessary to perform the export: ${permissionsNotGranted.joinToString(", ")}")
+        }
+    }
+
+    private fun saveToExcelFile(measurements: List<Measurement>) : String {
         Log.d("BPG", "Export to Excel started")
 
         val directory = File("${BpgApplication.instance.filesDir}")
         val csvFile = "${fileNameDateFormatter.format(Date())} BPG export.xls"
+        val fullPath = "${directory}/{$csvFile}"
 
         if (!directory.isDirectory) {
             Log.d("BPG", "Creating export directory")
@@ -38,6 +99,7 @@ object ExcelExporter {
 
         Log.d("BPG", "Directory path: $directory")
         Log.d("BPG", "File name: $csvFile")
+        Log.d("BPG", "Full path: $fullPath")
 
         val file = File(directory, csvFile)
         val wbSettings: WorkbookSettings = WorkbookSettings()
@@ -69,6 +131,6 @@ object ExcelExporter {
 
         Log.d("BPG", "Export completed")
 
-        return "$directory/$file"
+        return "${BpgApplication.instance.filesDir.path}/${csvFile}"
     }
 }
--- app/src/main/java/pl/sebcel/bpg/ui/measurementlist/MeasurementListActivity.kt
+++ app/src/main/java/pl/sebcel/bpg/ui/measurementlist/MeasurementListActivity.kt
@@ -1,11 +1,7 @@
 package pl.sebcel.bpg.ui.measurementlist
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -24,10 +20,8 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -39,16 +33,11 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 import pl.sebcel.bpg.R
 import pl.sebcel.bpg.data.di.FakeMeasurementRepository
-import pl.sebcel.bpg.data.local.database.model.Measurement
 import pl.sebcel.bpg.export.ExcelExporter
 import pl.sebcel.bpg.services.dataupdate.DataUpdateServiceScheduler
 import pl.sebcel.bpg.ui.measurementadd.MeasurementAddActivity
@@ -101,7 +90,7 @@ class MeasurementListActivity : ComponentActivity() {
                     }
                     if (items is MeasurementListUiState.Success) {
                         val measurements = (items as MeasurementListUiState.Success).data
-                        FloatingActionButton(onClick = { exportToExcel(measurements, snackbarHostState, scope) }) {
+                        FloatingActionButton(onClick = { ExcelExporter.exportToExcel(measurements, snackbarHostState, scope, this@MeasurementListActivity) }) {
                             Icon(
                                 Icons.Default.Share,
                                 contentDescription = getString(R.string.export_button_label)
@@ -153,60 +142,5 @@ class MeasurementListActivity : ComponentActivity() {
         startActivity(intent)
     }
 
-    private fun askForPermissions(permissions: Array<String>, requestCode: Int) {
-        Log.d("BPG", "Checking permissions")
-        val permissionsToRequest : MutableList<String> = mutableListOf<String>()
-        permissions.forEach {
-            if (ContextCompat.checkSelfPermission(this@MeasurementListActivity, it) == PackageManager.PERMISSION_GRANTED) {
-                Log.d("BPG", "Permission $it is granted")
-            } else {
-                Log.d("BPG", "Permission $it is not granted")
-                permissionsToRequest.add(it)
-            }
-        }
 
-        Log.d("BPG", "Requesting permissions")
-        ActivityCompat.requestPermissions(this@MeasurementListActivity, permissions, requestCode)
-
-        Log.d("BPG", "Checking if permissions were granted")
-        val permissionsNotGranted : MutableList<String> = mutableListOf()
-        permissions.forEach {
-            if (ContextCompat.checkSelfPermission(this@MeasurementListActivity, it) == PackageManager.PERMISSION_GRANTED) {
-                Log.d("BPG", "Permission $it is granted")
-            } else {
-                Log.d("BPG", "Permission $it is not granted")
-                permissionsNotGranted.add(it)
-            }
-        }
-
-        if (permissionsNotGranted.isNotEmpty()) {
-            throw Exception("Failed to get permissions necessary to perform the export: ${permissionsNotGranted.joinToString(", ")}")
-        }
-    }
-
-    private fun exportToExcel(measurements: List<Measurement>, snackbarHostState: SnackbarHostState, scope: CoroutineScope) {
-        val requestId = 1
-
-        try {
-            askForPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), requestId)
-            val fileName = ExcelExporter.export(measurements)
-            scope.launch {
-                val result = snackbarHostState.showSnackbar(message = getString(R.string.export_completed_toast_text) + fileName, duration = SnackbarDuration.Long, actionLabel = "Otwórz")
-                when (result) {
-                    SnackbarResult.ActionPerformed -> {
-
-                    }
-                    SnackbarResult.Dismissed -> {
-
-                    }
-                }
-
-            }
-        } catch (ex: Exception) {
-            scope.launch {
-                snackbarHostState.showSnackbar(message = getString(R.string.export_failed_toast_text) + ex.message, duration = SnackbarDuration.Long)
-            }
-            Log.e("BPG", "Export failed: ${ex.message}", ex)
-        }
-    }
 }
